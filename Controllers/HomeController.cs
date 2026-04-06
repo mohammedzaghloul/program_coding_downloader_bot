50e00bdbf1c1078c51e6b6157766cdd1ab339a2f
mohammedzaghloul <145876544+mohammedzaghloul@users.noreply.github.com>
2026-04-06 22:26:29 +0200

    Add controllers and models

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PremiumDownloader.Models;
+
+namespace PremiumDownloader.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View("Landing");
+    }
+
+    public IActionResult Privacy()
+    {
+        return View("PrivacyPolicy");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
